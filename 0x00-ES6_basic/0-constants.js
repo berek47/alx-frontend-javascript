d8cb571644416d7e5292415ab11eb116a3f920c5
berek47 <106895952+berek47@users.noreply.github.com>
2023-04-17 15:54:27 +0300

    0x00-ES6_basic

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,6 +1,6 @@
 export function taskFirst() {
-  const taskFirst = 'I prefer const when I can';
-  return taskFirst;
+  const task = 'I prefer const when I can';
+  return task;
 }
 
 export function getLast() {
